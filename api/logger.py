aa6b42943e79846dd2ffec28a62c3631882ee623
Alex Kemp <alexander.kemp@stfc.ac.uk>
2024-10-31 17:13:38 +0000

    Separate logging file

---
+++ api/logger.py
@@ -0,0 +1,35 @@
+import logging
+import json
+
+
+class JSONFormatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "timestamp": self.formatTime(record),
+            "module": record.module,
+            "app": "fastapi",
+        }
+        return json.dumps(log_record)
+
+
+def setup_logger():
+    logger = logging.getLogger("app_logger")
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(JSONFormatter())
+    logger.addHandler(handler)
+
+    # Configure Uvicorn’s access and error loggers to use JSONFormatter
+    uvicorn_logger = logging.getLogger("uvicorn")
+    uvicorn_logger.handlers = [handler]
+
+    access_logger = logging.getLogger("uvicorn.access")
+    access_logger.handlers = [handler]
+
+    return logger
+
+
+app_logger = setup_logger()
