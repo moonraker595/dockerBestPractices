beabecd91dd271386c557e3114be397b1599325c
Alex Kemp <alexander.kemp@stfc.ac.uk>
2024-10-31 17:13:53 +0000

    Added metrics

--- api/main.py
+++ api/main.py
@@ -1,40 +1,23 @@
-import logging
+from api.logger import app_logger
 from fastapi import FastAPI
 from api.icat_queries import icatQueries
-import json
 
 app = FastAPI()
-
-
-class JSONFormatter(logging.Formatter):
-    def format(self, record):
-        log_record = {
-            "level": record.levelname,
-            "message": record.getMessage(),
-            "timestamp": self.formatTime(record),
-            "module": record.module,
-            "source": "fastapi"  # Add custom source field
-        }
-        return json.dumps(log_record)
-
-
-# Apply the custom formatter
-handler = logging.StreamHandler()
-handler.setFormatter(JSONFormatter())
-
-logger = logging.getLogger("app_logger")
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-
-
-logger.info("starting")
+app_logger.info("Application started")
+hit_counter = {"endpoint_hits": 0}
 
 
 @app.get("/icat/{username}")
-async def say_hello(username: str):
+async def get_user(username: str):
+    hit_counter["endpoint_hits"] += 1
+    app_logger.info("Get user endpoint hit")
     # login to ICAT
     icatQueries.login()
     # get the user from icat
     user = icatQueries.get("User", "name", username)
-    logger.info(f"TEST")
     return user.instance
+
+
+@app.get("/metrics")
+def metrics():
+    return hit_counter
