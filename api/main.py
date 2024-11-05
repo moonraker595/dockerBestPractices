4c4dcbb91638ac4d0f9ee7dd5e4e76861822014d
Alex Kemp <alexander.kemp@stfc.ac.uk>
2024-11-05 12:03:17 +0000

    Added open API spec

--- api/main.py
+++ api/main.py
@@ -5,30 +5,41 @@ from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 from starlette.responses import Response
 from api.config import settings
 
-app = FastAPI()
+# By default, sets up OpenAPI documentation for the APP at /docs
+app = FastAPI(
+    title="ICAT API",
+    description="An example API for querying user information from the ICAT server",
+    version=settings.version,
+    contact={
+        "name": "Alexander Kemp",
+    }
+)
+
 app_logger.info("Application started")
 
 # Define a global counter for endpoint hits
 endpoint_hits_counter = Counter('endpoint_hits', 'Count of hits on the /icat endpoint')
 
 
-@app.get("/icat/{username}")
+@app.get("/icat/{username}", summary="Get user information",
+         description="Returns user information from the ICAT server")
 async def get_user(username: str):
     endpoint_hits_counter.inc()
-    app_logger.info("Get user endpoint hit")
+    app_logger.info("endpoint hit")
     # login to ICAT
     icatQueries.login()
     # get the user from icat
     user = icatQueries.get("User", "name", username)
+    # return user info
     return user.instance
 
 
-@app.get("/metrics")
+@app.get("/metrics", summary="Get metrics", description="Returns Prometheus metrics for the application")
 async def metrics():
     data = generate_latest()
     return Response(content=data, media_type=CONTENT_TYPE_LATEST)
 
 
-@app.get("/version")
+@app.get("/version", summary="Get API version", description="Returns the current version of the API")
 async def version():
     return {"version": settings.version}
